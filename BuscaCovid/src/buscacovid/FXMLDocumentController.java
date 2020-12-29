33db8e49b15921509a4f2398250732e7ce9ffc20
Escarlet <sakutwf@gmail.com>
2020-12-29 18:18:45 -0300

    Cambio de interfaz al escoger dificultad principiante

--- BuscaCovid/src/buscacovid/FXMLDocumentController.java
+++ BuscaCovid/src/buscacovid/FXMLDocumentController.java
@@ -5,13 +5,21 @@
  */
 package buscacovid;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -27,18 +35,15 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button avanzado;
     
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
 
     @FXML
-    private void nivelPrincipiante(ActionEvent event) {
+    private void nivelPrincipiante(ActionEvent event) throws IOException {
+        cambioEscena("interfazMatrices.fxml");
+        
     }
 
     @FXML
@@ -47,6 +52,25 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void nivelAvanzado(ActionEvent event) {
+    }
+    
+    public void cambioEscena(String nombreFxml) throws IOException{
+        Stage stage2 = (Stage) this.principiante.getScene().getWindow();
+        stage2.close();
+        Stage stage = new Stage();
+        Parent root2 = FXMLLoader.load(getClass().getResource(nombreFxml));
+        Scene scene = new Scene(root2);
+
+        stage.setScene(scene);
+        stage.show();
+
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
     
 }
